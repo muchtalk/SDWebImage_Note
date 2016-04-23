0cfb123190c3d66d023ccec150f9e5adf0f708bf
xxgg <469768777@qq.com>
2016-04-23 21:42:27 +0800

    第一次上传部分 注释

---
+++ SDWebImage_Note/SDWebImage_Note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImage_Note
+//
+//  Created by HG on 16/4/20.
+//  Copyright © 2016年 com.HG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDWebImage_Note/SDWebImage_Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImage_Note
+//
+//  Created by HG on 16/4/20.
+//  Copyright © 2016年 com.HG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
